a1a24838b14f60bbc3c6da3e059b5d1d3d197ad1
Rauan Amantaiuly <r.amantayuly@a1qa.com>
2024-07-02 18:25:30 +0500

    initial commit

--- src/test/java/appium/aquality/tests/BaseTest.java
+++ src/test/java/appium/aquality/tests/BaseTest.java
@@ -1,5 +1,22 @@
 package appium.aquality.tests;
 
-public class BaseTest {
+import aquality.appium.mobile.application.IMobileApplication;
+import aquality.appium.mobile.application.AqualityServices;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
+public class BaseTest {
+    protected IMobileApplication application;
+
+    @BeforeClass
+    public void setUp() {
+        application = AqualityServices.getApplication();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (application != null) {
+            application.quit();
+        }
+    }
 }
--- src/test/java/appium/aquality/tests/HomeWorkTwo.java
+++ src/test/java/appium/aquality/tests/HomeWorkTwo.java
@@ -1,16 +1,22 @@
 package appium.aquality.tests;
 
-import appium.testNG.BaseTest;
+import appium.aquality.screens.WelcomeScreen;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HomeWorkTwo extends BaseTest {
 
     @Test
     public void searchingForItems() {
+        WelcomeScreen welcomeScreen = new WelcomeScreen();
+        Assert.assertTrue(welcomeScreen.state().waitForDisplayed(), "Welcome screen is not displayed on app launch");
 
-    }
+        application.terminate("org.joinmastodon.android");
+//        Assert.assertFalse(application.isStarted(), "App is still running after termination");
 
-    @Test
-    public void interactionWithElements() {
+        application.activate("org.joinmastodon.android");
+        Assert.assertTrue(application.isStarted(), "App did not restart");
+
+        Assert.assertTrue(welcomeScreen.state().waitForDisplayed(), "Welcome screen is not displayed after reopening the app");
     }
 }
